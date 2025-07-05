4b68e81f8538fc32475bdd815c804bb34aacea1a
Lahiru De Zoysa <74654962+lahirudezoysa@users.noreply.github.com>
2025-07-05 13:20:05 +0530

    update on forked repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,15 @@ import HotelCard from "./HotelCard";
 import User from "./Manupa";
 import { Button } from "./components/ui/button";
 import { hotels } from "./data";
+import Navigation from "./components/Navigation";
+import {Hero} from "./components/Hero";
 
 function App() {
   return (
-    <div className="py-5 border-2 border-black">
-      {hotels.map((hotel, i) => {
-        return (
-          <HotelCard
-            key={hotel._id}
-            name={hotel.name}
-            location={hotel.location}
-            rating={hotel.rating}
-            reviews={hotel.reviews}
-            price={hotel.price}
-          />
-        );
-      })}
-      <Button onClick={() => console.log("Clicked")}>Click Me</Button>
-    </div>
+    <>
+      <Navigation />
+      <Hero />
+    </>
   );
 }
 
